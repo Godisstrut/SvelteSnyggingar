5a4c3f3e11865f2b9a4d23731d2f181c9ec69386
oblomdell <128600290+oblomdell@users.noreply.github.com>
2024-05-17 14:52:28 +0200

    Ändrade buttonprops till .restProps

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -1,3 +1,3 @@
 <header>
-    <h1 class="mb-4 text-3xl font-extrabold font-bold mt-4 text-4xl text-center text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Geniusguessr</span></h1>
+    <h1 class="mb-4 text-3xl font-extrabold font-bold mt-4 text-4xl text-center text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">GeniusGuessr</span></h1>
 </header>
--- my-app/src/components/Navigation.svelte
+++ my-app/src/components/Navigation.svelte
@@ -2,7 +2,7 @@
     import Button from "./Button.svelte"
 </script>
 
-<nav class="flex justify-center mt-5 pt-20 gap-20 ">
-    <Button props="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">TriviaGuessr</Button>
-    <Button props="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
+<nav class="flex justify-center mt-5 pt-20 gap-20">
+    <Button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">TriviaGuessr</Button>
+    <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
 </nav>
