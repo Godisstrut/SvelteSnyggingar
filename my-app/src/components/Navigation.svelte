ccade36983d3777abb3974653b71657453456f5f
Godisstrut <erikh13255@gmail.com>
2024-05-17 14:50:15 +0200

    Skapade ny text komponent

--- my-app/src/components/Navigation.svelte
+++ my-app/src/components/Navigation.svelte
@@ -1,8 +1,15 @@
 <script>
     import Button from "./Button.svelte"
+    import TextInfo from "./TextInfo.svelte";
 </script>
 
 <nav class="flex justify-center mt-5 pt-20 gap-20 ">
     <Button props="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">TriviaGuessr</Button>
     <Button props="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
 </nav>
+
+<p class="flex justify-center mt-5 gap-20 ">
+    <TextInfo props="text-sky-400 font-bold  "> Lite info om Trivia</TextInfo>
+    <TextInfo props="text-sky-400 font-bold ">Lite info om Lyrics</TextInfo>
+</p>
+
---
+++ my-app/src/components/TextInfo.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let props = "";
+</script>
+
+<p class={props}>
+    <slot />
+</p>
