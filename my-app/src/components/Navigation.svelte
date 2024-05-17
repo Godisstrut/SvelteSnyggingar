afb0b517a85875b860a880974e7a1ad584aaedcf
Godisstrut <erikh13255@gmail.com>
2024-05-17 13:01:32 +0200

    Header uppdatering
    Ändra hoverfärg på en knapp och justerade Sidans titel

--- my-app/src/components/Navigation.svelte
+++ my-app/src/components/Navigation.svelte
@@ -2,7 +2,7 @@
     import Button from "./Button.svelte"
 </script>
 
-<nav class="flex justify-center mt-5 gap-20">
+<nav class="flex justify-center mt-5 pt-20 gap-20 ">
     <Button props="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">TriviaGuessr</Button>
-    <Button props="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
+    <Button props="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
 </nav>
