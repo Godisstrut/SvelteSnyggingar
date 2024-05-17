d67ceb52b9408a4bc704051930fe27de49416821
Godisstrut <erikh13255@gmail.com>
2024-05-17 15:33:23 +0200

    Uppdaterade knappar
    Gjorde lite små justeringar på framsidans knappar

--- my-app/src/components/Navigation.svelte
+++ my-app/src/components/Navigation.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <nav class="flex justify-center mt-5 pt-20 gap-20 ">
-    <Button props="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">TriviaGuessr</Button>
-    <Button props="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">LyricGuessr</Button>
+    <Button props="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-8 rounded w-44 h-18">TriviaGuessr</Button>
+    <Button props="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-8 rounded w-44 h-18">LyricGuessr</Button>
 </nav>
 
-<p class="flex justify-center mt-5 gap-20 ">
-    <TextInfo props="text-sky-400 font-bold  "> Lite info om Trivia</TextInfo>
-    <TextInfo props="text-sky-400 font-bold ">Lite info om Lyrics</TextInfo>
+<p class="flex justify-center mt-2 gap-20 ">
+    <TextInfo props="text-sky-400 font-bold p-5 ">Klassiskt Trivia-spel</TextInfo>
+    <TextInfo props="text-sky-400 font-bold p-5">Gissa låttext-spelet</TextInfo>
 </p>
