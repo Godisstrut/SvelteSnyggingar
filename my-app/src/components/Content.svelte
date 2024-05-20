17fdb0f9567c6152d86c12ec4372b3b3aa1e4baa
Godisstrut <erikh13255@gmail.com>
2024-05-20 10:58:06 +0200

    Modal och Content
    La till två nya komponenter, Modal och Content

---
+++ my-app/src/components/Content.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div id="default-modal" class="backdrop flex justify-center mt-20 bg-yellow-400 h-60" >
+    <div class="modal"></div>
+    <h1 class="self-center">Vi erbjuder 2 olika spel lägen</h1>
+</div>
+
--- my-app/src/components/Navigation.svelte
+++ my-app/src/components/Navigation.svelte
@@ -1,6 +1,9 @@
 <script>
     import Button from "./Button.svelte"
     import TextInfo from "./TextInfo.svelte";
+    import Modal from './Modal.svelte';
+    import Content from "./Content.svelte";
+
 </script>
 
 <nav class="flex justify-center mt-5 pt-20 gap-20 ">
@@ -8,7 +11,10 @@
     <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-8 rounded w-44 h-18">LyricGuessr</Button>
 </nav>
 
-<p class="flex justify-center mt-2 gap-20 ">
+<div class="flex justify-center mt-2 gap-20 ">
     <TextInfo class="text-sky-400 font-bold p-5 ">Klassiskt Trivia-spel</TextInfo>
     <TextInfo class="text-sky-400 font-bold p-5">Gissa låttext-spelet</TextInfo>
-</p>
+</div>
+
+<Content />
+<Modal />
