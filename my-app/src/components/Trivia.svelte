d0c190f0dc104558925004196696501c508161c7
Godisstrut <erikh13255@gmail.com>
2024-05-23 14:02:48 +0200

    Färg styling
    Ändrade om till mer passande färger på knappar och texter

--- my-app/src/components/Trivia.svelte
+++ my-app/src/components/Trivia.svelte
@@ -74,34 +74,34 @@
         const resultScore = (score / total) * 10;
 
         if (resultScore === 10) {
-            return '<span class="text-amber-600 font-bold">Utmärkt! Alla rätt, snyggt jobbat!</span>';
+            return '<span class="text-xl text-amber-600 font-bold">Utmärkt! Alla rätt, snyggt jobbat!</span>';
         } else if (resultScore >= 7) {
-            return '<span class="text-sky-500 font-bold">Grymt jobbat! Nästan alla rätt</span>';
+            return '<span class="text-xl text-sky-700 font-bold">Grymt jobbat! Nästan alla rätt</span>';
         } else if (resultScore >= 5) {
-            return '<span class="text-yellow-400 font-bold">Bra jobbat!</span>';
+            return '<span class="text-xl text-yellow-500 font-bold">Bra jobbat!</span>';
         } else if (resultScore >= 1) {
-            return '<span class="text-orange-500 font-bold">Bättre lycka nästa gång!</span>';
+            return '<span class="text-xl text-red-700 font-bold">Bättre lycka nästa gång!</span>';
         } else {
-            return '<span class="text-red-400 font-bold">Du fick inga rätt, fick du sitta längst fram i klassrummet ofta när du var liten?</span>';
+            return '<span class="text-xl text-red-950 font-bold">Du fick inga rätt, fick du sitta längst fram i klassrummet ofta när du var liten?</span>';
         }
     }
 
 </script>
 
 <div>
-    <h1 class="mt-20 text-2xl font-extrabold flex justify-center text-gray-900 dark:text-white md:text-2xl lg:text-4xl"><span class="text-transparent pb-10 bg-clip-text bg-gradient-to-r to-emerald-500 from-sky-300">Trivia Frågor:</span></h1>
+    <h1 class="mt-20 text-2xl font-extrabold flex justify-center text-gray-900 dark:text-white md:text-2xl lg:text-4xl"><span class="text-transparent pb-10 bg-clip-text bg-gradient-to-r to-emerald-500 from-sky-300">TriviaSpel! Utformat för alla åldrar</span></h1>
     {#if !quizStarted}
         {#if !questionType}
             <div class="flex flex-col items-center gap-4 mt-10">
-                <p class="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">Välj frågetyp</p>
+                <p class="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">Välj frågetyp:</p>
                 <nav class="flex justify-center mt-5 space-x-5">
                     <Button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => selectedQuestionType('boolean')}>Sant/Falskt</Button>
-                    <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => selectedQuestionType('multiple')}>Flerval</Button>
+                    <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => selectedQuestionType('multiple')}>Flervals</Button>
                 </nav>
             </div>
         {:else}
-            <div class="flex flex-col items-center gap-4 mt-20">
-                <p class="text-xl font-semibold">Välj svårighetsgrad:</p>
+            <div class="flex flex-col items-center gap-4 mt-10">
+                <p class="text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">Välj svårighetsgrad:</p>
                 <nav class="flex justify-center mt-5 space-x-5">
                     <Button class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => quizDifficulty('easy')}>Easy</Button>
                     <Button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => quizDifficulty('medium')}>Medium</Button>
@@ -121,7 +121,7 @@
                 {:else if questionType === 'multiple'}
                     <div class="button-container flex justify-center mt-4 space-x-8">
                         {#each [...triviaData[currentIndex].incorrect_answers, triviaData[currentIndex].correct_answer].sort() as answer}
-                            <Button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" on:click={() => submitAnswer(answer)}>{answer}</Button>
+                            <Button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-4 px-8 rounded-full" on:click={() => submitAnswer(answer)}>{answer}</Button>
                         {/each}
                     </div>
                 {/if}
@@ -131,7 +131,7 @@
         {/if}
     {:else}
         <div class="flex flex-col items-center gap-10 pt-20 bg-cyan-400 h-80">
-            <p class="font-medium mt-10">Ditt resultat är {score} ut av {triviaData.length}</p>
+            <p class="text-xl font-semibold mt-10">Ditt resultat är {score} ut av {triviaData.length}</p>
             <p class="font-normal mt-5 text-center">{@html resultMessage(score, triviaData.length)}</p>
         </div>
     {/if}
