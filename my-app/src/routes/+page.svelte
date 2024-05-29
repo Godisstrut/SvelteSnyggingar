6ce6aaec198a2821917a344b4cdd9909265f9741
oblomdell <128600290+oblomdell@users.noreply.github.com>
2024-05-29 14:23:05 +0200

    Översatte till svenska
    Översatte front-end delen till svenska för att hålla allt konsekvent.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -14,25 +14,15 @@
 </script>
 
 <div class="flex flex-col items-center mt-4">
-    <h1 class="text-xl font-bold mb-4">Results from Musixmatch</h1>
+    <h1 class="text-xl font-bold mb-4">LyricsGuessr:</h1>
     <ul class="flex flex-col space-y-2">
         {#each storedAnswers as answers}
             <li class="border border-gray-300 p-2 rounded shadow">
-                <h2 class="text-lg font-bold">Quiz Result</h2>
+                <h2 class="text-lg font-bold">Quiz Resultat</h2>
                 <ul class="ml-4 mt-2">
                     {#each answers as { guess, answer, isCorrect }}
-                        <li
-                            class="{isCorrect
-                                ? 'bg-green-200'
-                                : 'bg-red-200'} border border-gray-300 p-2 rounded shadow mt-2"
-                        >
-                            <strong
-                                >{isCorrect
-                                    ? "Guessed"
-                                    : "Wrong guess"}:</strong
-                            >
-                            {guess}, <strong>Correct:</strong>
-                            {answer}
+                        <li class="{isCorrect ? 'bg-green-200' : 'bg-red-200'} border border-gray-300 p-2 rounded shadow mt-2">
+                            <strong>Ditt svar:</strong> {guess}, <strong>Rätt svar:</strong> {answer}
                         </li>
                     {/each}
                 </ul>
